cd4da7db819626fc6d8e400848dccaa0894a6abc
shershen08 <michail.kuznetsov@gmail.com>
2025-12-25 09:56:03 +0000

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="Spotify playlists Backend")
 # CORS middleware for Next.js frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
